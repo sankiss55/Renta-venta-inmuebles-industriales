c7bfea446d8bbd8871b958c12e578155ec5eb535
sanprogramer <sanchezvera490@gmail.com>
2025-06-28 14:13:12 -0600

    base pagina TBC

---
+++ JS/main.js
@@ -0,0 +1,64 @@
+ const menuToggle = document.getElementById('menuToggle');
+        const navLinks = document.getElementById('navLinks');
+
+        menuToggle.addEventListener('click', () => {
+            navLinks.classList.toggle('active');
+        });
+
+        // Close menu when clicking on links
+        document.querySelectorAll('.nav-links a').forEach(link => {
+            link.addEventListener('click', () => {
+                navLinks.classList.remove('active');
+            });
+        });
+
+        // Smooth scroll
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                const target = document.querySelector(this.getAttribute('href'));
+                if (target) {
+                    target.scrollIntoView({
+                        behavior: 'smooth',
+                        block: 'start'
+                    });
+                }
+            });
+        });
+         const scrollContainer = document.querySelector('.scroll_services');
+
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  scrollContainer.addEventListener('mousedown', (e) => {
+    isDown = true;
+    scrollContainer.classList.add('active');
+    startX = e.pageX - scrollContainer.offsetLeft;
+    scrollLeft = scrollContainer.scrollLeft;
+  });
+
+  scrollContainer.addEventListener('mouseleave', () => {
+    isDown = false;
+  });
+
+  scrollContainer.addEventListener('mouseup', () => {
+    isDown = false;
+  });
+
+  scrollContainer.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - scrollContainer.offsetLeft;
+    const walk = (x - startX) * 1.5; 
+    scrollContainer.scrollLeft = scrollLeft - walk;
+  });
+  setInterval(()=>{
+    const scrollContainer = document.querySelector('.scroll_services');
+    if (scrollContainer) {
+        scrollContainer.scrollLeft += 500; 
+        if (scrollContainer.scrollLeft >= scrollContainer.scrollWidth - scrollContainer.clientWidth) {
+            scrollContainer.scrollLeft = 0; 
+        }
+    }
+  }, 8000);
